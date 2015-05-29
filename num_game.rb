d13e95f0ee6c018d8d60f4b3354631fd0e5287a5
Jonathan Wong <18wongj@gmail.com>
2015-05-28 20:05:10 -0400

    Create num_game.rb

---
+++ num_game.rb
@@ -0,0 +1,14 @@
+#This is copyright Jonathan Wong. To play this game you need a terminal and ruby installed.
+puts ""
+puts  "                        Welcome to NumberGame by Jonathan Wong"
+puts ""
+puts "                       Guess a number from 1-10 to get the flag!"
+guess = gets.to_i
+answer = rand(10)
+if guess == answer
+    answerexp= guess.to_s
+    puts "yay you got it right, the answer was "+answerexp
+elsif guess != answer
+    answerexp= answer.to_s
+    puts "no... the answer was "+answerexp
+end    

6ed8c9e9091907d898bf7fc4d652cdcd7d064d66
Jonathan Wong <18wongj@gmail.com>
2015-06-22 18:43:10 -0400

    Update num_game.rb

--- num_game.rb
+++ num_game.rb
@@ -2,7 +2,7 @@
 puts ""
 puts  "                        Welcome to NumberGame by Jonathan Wong"
 puts ""
-puts "                       Guess a number from 1-10 to get the flag!"
+puts "                       Guess a number from 0-10 to get the flag!"
 guess = gets.to_i
 answer = rand(10)
 if guess == answer
